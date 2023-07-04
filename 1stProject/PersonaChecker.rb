895f4d3212c2de5b9e420744ff85150b0984ad6d
Thaís Leoni <92694950+ThLeoni@users.noreply.github.com>
2023-07-04 21:23:59 +0100

    Create PersonaChecker.rb

---
+++ 1stProject/PersonaChecker.rb
@@ -0,0 +1,10 @@
+puts "Enter your name: "
+name = gets.chomp
+
+puts "Enter your last name: "
+lastname = gets.chomp
+
+puts "Enter your age: "
+age = gets.chomp.to_i
+
+puts "#{name} #{sobrenome} you are #{age}"
